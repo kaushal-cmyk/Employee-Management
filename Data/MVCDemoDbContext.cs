ee385bd8de3f216c0eb4151abcd1a6d1f26ad44e
Kaushal Adhikari <kaushaladhikari098@gmail.com>
2024-07-14 23:17:11 +0545

    First Commit

---
+++ Data/MVCDemoDbContext.cs
@@ -0,0 +1,16 @@
+﻿using CrudOperation.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection.Metadata.Ecma335;
+
+namespace CrudOperation.Data
+{
+	public class MVCDemoDbContext : DbContext
+	{
+		public MVCDemoDbContext(DbContextOptions options) : base(options)
+		{
+		}
+
+        public DbSet<Employee> Employees { get; set; } // Table name Employees is created. 
+    }
+}
+    
