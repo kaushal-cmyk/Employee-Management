f38bb43bcb1a1ca1731701925b31a97992d38bdd
Kaushal Adhikari <kaushaladhikari098@gmail.com>
2024-07-15 00:45:10 +0545

    Add Employee

--- Controllers/EmployeesController.cs
+++ Controllers/EmployeesController.cs
@@ -1,16 +1,52 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CrudOperation.Data;
+using CrudOperation.Models;
+using CrudOperation.Models.Domain;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace CrudOperation.Controllers
 {
 	public class EmployeesController : Controller
 	{
+        private readonly MVCDemoDbContext mvcDemoDbContext;
+
+        public EmployeesController(MVCDemoDbContext mvcDemoDbContext)
+        {
+            this.mvcDemoDbContext = mvcDemoDbContext;
+        }
+
 		[HttpGet]
+		public async Task<IActionResult> Index()
+		{
+			var employees =  await mvcDemoDbContext.Employees.ToListAsync();
+			
+		}
+
+        [HttpGet]
 		public IActionResult Add()
 		{
 			return View();
 		}
 
+		[HttpPost]
+		public async Task<IActionResult> Add(AddEmployeeViewModel addEmployeeRequest)
+		{
+			var employee = new Employee()
+			{
+				Id = Guid.NewGuid(),
+				Name = addEmployeeRequest.Name,
+				Email = addEmployeeRequest.Email,
+				Salary = addEmployeeRequest.Salary,
+				DateOfBirth = addEmployeeRequest.DateOfBirth,
+				Department = addEmployeeRequest.Department,
+			};
 
+			await mvcDemoDbContext.Employees.AddAsync(employee);
+			await mvcDemoDbContext.SaveChangesAsync();
+			return RedirectToAction("ADD");
+
+
+		}
 
 	}
 }
