2dbe7c1235dfbbb17ff33418e3edcfa25ebea84a
Kaushal Adhikari <kaushaladhikari098@gmail.com>
2024-07-20 23:48:49 +0545

    button

--- obj/Debug/net6.0/CrudOperation.AssemblyInfo.cs
+++ obj/Debug/net6.0/CrudOperation.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CrudOperation")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c0fe6fc41eac393586a6e8e41a12bd7cf2b16833")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+39a321ede79ef1326c3939c747b68269bff8be96")]
 [assembly: System.Reflection.AssemblyProductAttribute("CrudOperation")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CrudOperation")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
